952e1abc72c10971808a69290cf4e20858e6f836
Duncan Champney <DChampney@WareTo.com>
2023-12-05 10:26:50 -0500

    Initial commit

---
+++ FooAlertController/FooController.swift
@@ -0,0 +1,9 @@
+//
+//  FooController.swift
+//  FooAlertController
+//
+//  Created by Duncan Champney on 12/4/23.
+//
+
+import UIKit
+
--- FooAlertController/ViewController.swift
+++ FooAlertController/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FooController: UIAlertController {
+}
 
+class ViewController: UIViewController {
+    
+    let buttonLabels = [
+    """
+    Button1
+    line2
+    """,
+    """
+    Button2
+    line2
+    """,
+    """
+    Button3
+    line2
+    """
+    ]
+
+    @IBAction func handleAlertButton(_ sender: Any) {
+        presentAlert(type: UIAlertController.self)
+    }
+    
+    @IBAction func handleFooButton(_ sender: Any) {
+        presentAlert(type: FooController.self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        UILabel.appearance(whenContainedInInstancesOf: [FooController.self]).numberOfLines = 2
+    }
+    
+    func presentAlert(type: UIAlertController.Type) {
+        let sheet = type.init(title: type.description(), message: nil, preferredStyle: .actionSheet)
+            
+        for buttonTitle in buttonLabels {
+            
+            let item = UIAlertAction(title: buttonTitle, style: .default) { (action) in
+                print("Button \(buttonTitle) tapped")
+            }
+            sheet.addAction(item)
+        }
+
+        present(sheet, animated: true, completion: nil)
     }
 
+    
 
 }
 
